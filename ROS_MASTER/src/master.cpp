d063aaa8f6ec8cbc83db7d1ae7e1a8f0456a45d7
ed <f29993856@gmail.com>
2017-09-03 04:21:41 -0700

    add ROS_MASTER_API

---
+++ ROS_MASTER/src/master.cpp
@@ -0,0 +1,61 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+ros::master::V_TopicInfo rostopic_list;
+ros::V_string rosnode_list;
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "master");
+  ros::NodeHandle nh;
+
+  if(ros::ok())
+  {
+    while(!ros::master::check());
+    if(ros::master::getTopics(rostopic_list))
+    {
+      ROS_INFO("master is established");
+      ros::master::getTopics(rostopic_list);
+      ros::master::getNodes(rosnode_list);
+      //rostopic list
+
+      const std::string host_info=ros::master::getHost();
+      std::cout<<"---------ros host-------"<<std::endl;
+      std::cout<<"host name : "<<host_info.c_str()<<std::endl;
+
+      std::cout<<"---------port-------"<<std::endl;
+      uint32_t port_info=ros::master::getPort();
+      std::cout<<"port : "<<port_info<<std::endl;
+
+      std::cout<<"---------URI-------"<<std::endl;
+      std::string URI_info=ros::master::getURI();
+      std::cout<<"URI : "<<URI_info.c_str()<<std::endl;
+
+      std::cout<<"----rosnode list---"<<std::endl;
+      for(ros::V_string::iterator it = rosnode_list.begin() ; it !=rosnode_list.end() ; it++)
+      {
+        std::cout << "node" <<it-rosnode_list.begin()<<":"<<*(it) <<std::endl;
+      }
+
+      std::cout<<"----rostopic list---"<<std::endl;
+      for (ros::master::V_TopicInfo::iterator it = rostopic_list.begin() ; it != rostopic_list.end(); it++)
+      {
+        const ros::master::TopicInfo& info = *it;
+        std::cout << "topic_" << it - rostopic_list.begin()
+                  << ": " << info.name<<" | "
+                  <<"  type: "<< info.datatype
+                  << std::endl;
+      }
+
+
+
+    }else
+    {
+      ROS_ERROR("get topics error");
+    }
+  }else
+  {
+    ROS_ERROR("ros master is not exist");
+  }
+
+  return 0;
+}
